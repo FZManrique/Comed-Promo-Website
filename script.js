68719bb6f67468312be1b50e2e1d9f38b02ac88f
Franzen Zhaun Manrique <franzenzhaun.manrique@msugensan.edu.ph>
2023-01-28 17:28:16 +0800

    add content, update slideshow to use CSS, and such

--- script.js
+++ script.js
@@ -13,18 +13,5 @@
   yearEl.textContent = year;
 }
 {
-  let slideIndex = 0;
-  const slides = document.getElementsByClassName("slide__images");
-  showSlides()
 
-function showSlides() {
-  let i;
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000);
-}
 }
